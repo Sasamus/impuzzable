32fff5eb2d74ea127defb4db28ea2172b86055b1
Sasamus <labus93@gmail.com>
2015-02-07 12:08:37 +0100

    Started to implement class SearchAlgorithm

--- src/se/miun/dt015a/logic/model/impuzzable/SearchAlgorithm.java
+++ src/se/miun/dt015a/logic/model/impuzzable/SearchAlgorithm.java
@@ -1,26 +1,68 @@
 package se.miun.dt015a.logic.model.impuzzable;
 
+import java.util.List;
+
+import se.miun.dt015a.logic.model.Piece;
 import se.miun.dt015a.logic.model.Puzzle;
 import se.miun.dt015a.logic.model.ValidPuzzleSolution;
 
 /**
- * Implements an algorithm for solving the Impuzzable puzzle.
- * Define your own (smart/efficient) solve method.
+ * Implements an algorithm for solving the Impuzzable puzzle. Define your own
+ * (smart/efficient) solve method.
  *
  * You may want to create additional helper classes. However, you are not
  * allowed to alter the other existing classes.
  */
 public class SearchAlgorithm implements ImpuzzableAlgorithm {
 
-  // Add member fields if necessary.
+	/*
+	 * Ideas:
+	 * 
+	 * Get string versions of all pieces, maybe convert it to be easier to
+	 * handle. Check what pieces have the needed edge, on those check the other
+	 * edges if present. If all edges fit, place it. If no free pieces fit,
+	 * check if a placed piece will. If one does, move it there. Redo the
+	 * process on the newly made free space.
+	 * 
+	 * If the temporary puzzle is filled, place pieces in the actual puzzle.
+	 */
 
-  public SearchAlgorithm() {
-    // Add code here if necessary, but do not add other constructors.
-  }
+	// Add member fields if necessary.
+	
+	/**
+	 * Holds the size of X
+	 */
+	int sizeX;
+	
+	/**
+	 * Holds the size of Y
+	 */
+	int sizeY;
+	
+	/**
+	 * Holds the Piece objects
+	 */
+	List<Piece> pieces;
 
-  @Override
-  public ValidPuzzleSolution solve(Puzzle puzzle) {
-    return new RandomSearch().solve(puzzle); // Your code here.
-  }
+	public SearchAlgorithm() {
+		// Add code here if necessary, but do not add other constructors.
+	}
+
+	@Override
+	public ValidPuzzleSolution solve(Puzzle puzzle) {
+		
+		//Get puzzle's size
+		sizeX = puzzle.getSizeX();
+		sizeY = puzzle.getSizeY();
+		
+		//Get puzzle's Pieces
+		pieces = puzzle.getPieces();
+		
+		// Return a ValidPuzzleSolution object
+		return new ValidPuzzleSolution(puzzle, null);
+		
+		
+//		return new RandomSearch().solve(puzzle); // Your code here.
+	}
 
 }
